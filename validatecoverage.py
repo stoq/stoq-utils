1bec23f8767f6bd42c582ca33f9fc5ec682562c6
Thiago Bellini <hackedbellini@gmail.com>
2017-03-31 16:08:56 -0300

    Use float for current/expected values
    Converting the float from json to decimal will make it consider its arithmetic
    issues and the check for coverage would fail. For example, 95.65 in the json
    would be converted to 95.650000000000005684341886080801486968994140625, which
    is almost equal to the original value but greater in the comparison.
    
    Also improve the fixes output to the value expected in the json
    
    Change-Id: Iea1df470bafb2415a7cc80d45f5123572aa7b657

--- validatecoverage.py
+++ validatecoverage.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 
-import decimal
 import json
 import sys
 from xml.etree.ElementTree import parse
@@ -23,14 +22,14 @@ for module in sorted(coverage.findall('.//class')):
         continue
 
     modules[filename] = module
-    current = decimal.Decimal(module.attrib.get('line-rate')) * 100
-    expected = decimal.Decimal(exceptions.get(filename, 100))
+    current = float(module.attrib.get('line-rate')) * 100
+    expected = float(exceptions.get(filename, 100))
 
     if current != 100 and filename not in exceptions:
         errors.append("2: %s has a coverage of %s%%, but expected 100%%, "
                       "add it to %s:" % (
                           filename, current * 100, exceptions_fname))
-        fixes.append("    ('%s', '%.4f')," % (filename, current))
+        fixes.append(' "%s": %.2f' % (filename, current))
     elif current < expected:
         errors.append("1: %s has a coverage of %s%%, but expected %s%%, "
                       "add more tests or update %s" % (
@@ -52,8 +51,7 @@ for error in sorted(errors):
 
 if fixes:
     print "FIXES:"
-    for fix in sorted(fixes):
-        print fix
+    print ',\n'.join(sorted(fixes))
 
 
 sys.exit(len(errors))
