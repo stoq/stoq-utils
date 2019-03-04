a358df600460981e9b95ea637b7f189b95be30cd
Lucas Ferreira Fonseca <lfonseca010@gmail.com>
2019-03-04 14:22:05 -0300

    Track automated egg building scripts on source control
    The script that generates plugins' egg was moved from Jenkins
    to a source control folder, what give us access to its
    history and speeds up editing.
    
    Fixes: https://gitlab.com/stoqtech/private/bdil/issues/196
    Change-Id: Ia6fe76e82aa9fceb8886de6696e631c50cebf32b

---
+++ jenkins-egg-release.sh
@@ -0,0 +1,76 @@
+# generate egg artifacts if the build was triggered by
+# the merge of a gerrit change that updates the
+# version of the project
+echo "experimental build step, to generate eggs and a git tag whenever a plugin version change is merged"
+source venv/bin/activate
+source stoq/utils/env.sh
+cd $plugin_name
+
+echo $GERRIT_EVENT_TYPE
+
+# nfe, ntk, nofiscal plugin gets its version from a different file
+if [ $plugin_name = "stoq-plugin-nfe" ]; then
+  VERSION_FILE="stoqnfe/nfce.plugin"
+  VERSION=`grep "^Version=\(.*\)" "$VERSION_FILE" | sed "s/.*=//g"`
+elif [ $plugin_name = "stoq-plugin-ntk" ]; then
+  VERSION_FILE="stoqntk/ntk.plugin"
+  VERSION=`grep "^Version=\(.*\)" "$VERSION_FILE" | sed "s/.*=//g"`
+elif [ $plugin_name = "stoq-plugin-nonfiscal" ]; then
+  VERSION_FILE="stoqnonfiscal/nonfiscal.plugin"
+  VERSION=`grep "^Version=\(.*\)" "$VERSION_FILE" | sed "s/.*=//g"`
+elif [ $plugin_name = "stoq-plugin-sat" ]; then
+  VERSION_FILE="stoqsat/sat.plugin"
+  VERSION=`grep "^Version=\(.*\)" "$VERSION_FILE" | sed "s/.*=//g"`
+else
+  VERSION_FILE="setup.py"
+  VERSION=`grep "version=\"" setup.py |sed -e "s/.*=\"//g" -e "s/\".*//g"`
+fi
+
+if [ -z "$GERRIT_EVENT_TYPE" ]; then
+  echo "This is a manual build, generate an alpha egg."
+    MANUAL_BUILD=true
+    GIT_HASH=`git log --pretty=format:'%h' -n 1`
+  sed -e"s/version=\".*/version=\"$VERSION-alpha-$GIT_HASH\",/g" -i setup.py
+fi
+
+if [ $GERRIT_EVENT_TYPE = "change-merged" ]; then
+    # sat plugin is failing compilation, ignore the debugs
+    if [ $plugin_name = "stoq-plugin-sat" ]; then
+      echo "sat plugin"
+      sed -i -e"s/ -o/ --verbose -o/g" Makefile
+      env
+      uname -a
+      hostname
+      ip a
+  fi
+
+    git show
+
+    # check if last change modified a version line
+    VERSION_LINES=$(git show | grep -i "[^q]version=")
+    if [ -n "$VERSION_LINES" ]; then
+        echo "Plugin version changed to $VERSION"
+        VERSION_CHANGED=true
+        echo "VERSION_CHANGED=$VERSION_CHANGED"
+
+    # create git tag
+        if `git tag -a "$VERSION" -m "New release"`;then
+          echo 'new tag created'
+          git push origin --tags
+        else
+          echo 'ERROR creating tag (tag exists already? should we exit?) For now, create the egg for the existing tagged commit'
+          git checkout "$VERSION"
+        fi
+        git show
+
+    fi
+fi
+
+if [[ ($VERSION_CHANGED = true || $MANUAL_BUILD = true) ]]; then
+  make && make plugin-egg || true
+  ls dist
+else
+  echo "Version didn't change, no need for making an egg."
+    echo "VERSION_CHANGED=$VERSION_CHANGED MANUAL_BUILD=$MANUAL_BUILD"
+  exit 0
+fi
