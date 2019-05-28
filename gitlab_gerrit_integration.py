28fbf99549d885248fbd4d9d7279f7d9dfccf1ef
Lucas Fonseca <lfonseca010@gmail.com>
2019-05-28 16:43:43 -0300

    Integrate gerrit and gitlab
    When gerrit receives a commit, jenkins posts a comment on
    the gitlab issue with the gerrit's link.
    
    Fixes: https://gitlab.com/stoqtech/private/bdil/issues/345
    Change-Id: I44fd0c7486f699a65a261036103c81860ef7cae8

--- gitlab_gerrit_integration.py
+++ gitlab_gerrit_integration.py
@@ -6,28 +6,23 @@
 
 import re
 import requests
-import asyncio
 import os
-import aiohttp
-import json
-from gidgetlab.aiohttp import GitLabAPI
 
 
-async def main():
+def main():
     # Only add the comment to gitlab for the first patchset
     if int(os.getenv("GERRIT_PATCHSET_NUMBER")) > 1:
         return None
 
     data = os.getenv("GERRIT_CHANGE_COMMIT_MESSAGE")
     project, issue = re.search("Fixes: https://gitlab.com/(.*)/issues/(\d+)", data).groups()
-    assert project, project
-    assert issue, issue
-    gitlab_url = "/projects/{}/issues/{}/notes/".format(project.replace('/', '%2F'), issue)
+    assert project and issue, (project, issue)
 
-    async with aiohttp.ClientSession() as session:
-        gl = GitLabAPI(session, "devstoq", access_token=os.getenv("GITLAB_API_TOKEN"))
-        await gl.post(gitlab_url, data={"body": os.getenv("GERRIT_CHANGE_URL")})
+    gitlab_url = "/projects/{}/issues/{}/notes".format(project.replace('/', '%2F'), issue)
+    requests.post(gitlab_url,
+                  headers={"Private-Token": os.getenv("GITLAB_API_TOKEN")},
+                  data={"body": os.getenv("GERRIT_CHANGE_URL")})
+
 
 if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
+    main()
